87acd2741fffb578071fb52e4d4f793b11ba55b0
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-12-02 15:26:10 -0300

    commit curriculo skills

---
+++ curriculo-digital/curriculo_digital/src/App.jsx
@@ -0,0 +1,24 @@
+// REACT
+import Header from './components/Header/Header'
+import Summary from './components/Summary/Summary'
+import Contact from './components/Contact/Contact'
+import Skills from './components/Skills/Skills'
+// CSS
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div className='App'>
+      <Header/>
+      <Summary/>
+      <Contact/>
+      <Skills/>
+    </div>
+
+  )
+
+}
+
+export default App
